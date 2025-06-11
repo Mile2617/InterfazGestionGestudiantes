d0d5fb33fa72ec2f28ac093c83938018c248cf9a
start.vaadin.com <no-reply@vaadin.com>
2025-06-11 02:14:51 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/carreras/CarrerasView.java
@@ -0,0 +1,106 @@
+package com.example.application.views.carreras;
+
+import com.example.application.data.SamplePerson;
+import com.example.application.services.SamplePersonService;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H4;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("Carreras")
+@Route("my-view")
+@Menu(order = 2, icon = LineAwesomeIconUrl.USER_GRADUATE_SOLID)
+@Uses(Icon.class)
+public class CarrerasView extends Composite<VerticalLayout> {
+
+    public CarrerasView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        FormLayout formLayout3Col = new FormLayout();
+        TextField textField = new TextField();
+        TextField textField2 = new TextField();
+        ComboBox comboBox = new ComboBox();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        H4 h4 = new H4();
+        Grid multiSelectGrid = new Grid(SamplePerson.class);
+        VerticalLayout layoutColumn3 = new VerticalLayout();
+        Button buttonSecondary = new Button();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.setHeight("min-content");
+        formLayout3Col.setWidth("100%");
+        formLayout3Col.setResponsiveSteps(new ResponsiveStep("0", 1), new ResponsiveStep("250px", 2),
+                new ResponsiveStep("500px", 3));
+        textField.setLabel("Nombre de la Carrera");
+        textField.setWidth("min-content");
+        textField2.setLabel("Número de materias");
+        textField2.setWidth("min-content");
+        comboBox.setLabel("Tipo de Carrera");
+        comboBox.setWidth("min-content");
+        setComboBoxSampleData(comboBox);
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        h4.setText("Materias");
+        h4.setWidth("max-content");
+        multiSelectGrid.setSelectionMode(Grid.SelectionMode.MULTI);
+        multiSelectGrid.setWidth("100%");
+        multiSelectGrid.getStyle().set("flex-grow", "0");
+        setGridSampleData(multiSelectGrid);
+        layoutColumn3.setWidthFull();
+        getContent().setFlexGrow(1.0, layoutColumn3);
+        layoutColumn3.setWidth("100%");
+        layoutColumn3.getStyle().set("flex-grow", "1");
+        buttonSecondary.setText("Crear Nueva Carrera");
+        layoutColumn3.setAlignSelf(FlexComponent.Alignment.CENTER, buttonSecondary);
+        buttonSecondary.setWidth("960px");
+        getContent().add(layoutRow);
+        layoutRow.add(formLayout3Col);
+        formLayout3Col.add(textField);
+        formLayout3Col.add(textField2);
+        formLayout3Col.add(comboBox);
+        getContent().add(layoutColumn2);
+        layoutColumn2.add(h4);
+        layoutColumn2.add(multiSelectGrid);
+        getContent().add(layoutColumn3);
+        layoutColumn3.add(buttonSecondary);
+    }
+
+    record SampleItem(String value, String label, Boolean disabled) {
+    }
+
+    private void setComboBoxSampleData(ComboBox comboBox) {
+        List<SampleItem> sampleItems = new ArrayList<>();
+        sampleItems.add(new SampleItem("first", "First", null));
+        sampleItems.add(new SampleItem("second", "Second", null));
+        sampleItems.add(new SampleItem("third", "Third", Boolean.TRUE));
+        sampleItems.add(new SampleItem("fourth", "Fourth", null));
+        comboBox.setItems(sampleItems);
+        comboBox.setItemLabelGenerator(item -> ((SampleItem) item).label());
+    }
+
+    private void setGridSampleData(Grid grid) {
+        grid.setItems(query -> samplePersonService.list(VaadinSpringDataHelpers.toSpringPageRequest(query)).stream());
+    }
+
+    @Autowired()
+    private SamplePersonService samplePersonService;
+}
---
+++ src/main/java/com/example/application/views/estudiantes/EstudiantesView.java
@@ -0,0 +1,104 @@
+package com.example.application.views.estudiantes;
+
+import com.example.application.data.SamplePerson;
+import com.example.application.services.SamplePersonService;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
+import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("Estudiantes")
+@Route("")
+@Menu(order = 0, icon = LineAwesomeIconUrl.ADDRESS_CARD_SOLID)
+@Uses(Icon.class)
+public class EstudiantesView extends Composite<VerticalLayout> {
+
+    public EstudiantesView() {
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        FormLayout formLayout3Col = new FormLayout();
+        TextField textField = new TextField();
+        TextField textField2 = new TextField();
+        TextField textField3 = new TextField();
+        FormLayout formLayout2Col = new FormLayout();
+        TextField textField4 = new TextField();
+        ComboBox comboBox = new ComboBox();
+        Button buttonPrimary = new Button();
+        Grid stripedGrid = new Grid(SamplePerson.class);
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutColumn2.addClassName(Padding.SMALL);
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        formLayout3Col.setWidth("100%");
+        formLayout3Col.setResponsiveSteps(new ResponsiveStep("0", 1), new ResponsiveStep("250px", 2),
+                new ResponsiveStep("500px", 3));
+        textField.setLabel("Nombre");
+        textField.setWidth("min-content");
+        textField2.setLabel("Apellido");
+        textField2.setWidth("min-content");
+        textField3.setLabel("Número Telefónico");
+        textField3.setWidth("min-content");
+        formLayout2Col.setWidth("100%");
+        textField4.setLabel("e-mail");
+        textField4.setWidth("min-content");
+        comboBox.setLabel("Nivel de estudio");
+        comboBox.setWidth("min-content");
+        setComboBoxSampleData(comboBox);
+        buttonPrimary.setText("Crear Nuevo estudiante");
+        layoutColumn2.setAlignSelf(FlexComponent.Alignment.CENTER, buttonPrimary);
+        buttonPrimary.setWidth("970px");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        stripedGrid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
+        stripedGrid.setWidth("100%");
+        stripedGrid.getStyle().set("flex-grow", "0");
+        setGridSampleData(stripedGrid);
+        getContent().add(layoutColumn2);
+        layoutColumn2.add(formLayout3Col);
+        formLayout3Col.add(textField);
+        formLayout3Col.add(textField2);
+        formLayout3Col.add(textField3);
+        layoutColumn2.add(formLayout2Col);
+        formLayout2Col.add(textField4);
+        formLayout2Col.add(comboBox);
+        layoutColumn2.add(buttonPrimary);
+        layoutColumn2.add(stripedGrid);
+    }
+
+    record SampleItem(String value, String label, Boolean disabled) {
+    }
+
+    private void setComboBoxSampleData(ComboBox comboBox) {
+        List<SampleItem> sampleItems = new ArrayList<>();
+        sampleItems.add(new SampleItem("first", "First", null));
+        sampleItems.add(new SampleItem("second", "Second", null));
+        sampleItems.add(new SampleItem("third", "Third", Boolean.TRUE));
+        sampleItems.add(new SampleItem("fourth", "Fourth", null));
+        comboBox.setItems(sampleItems);
+        comboBox.setItemLabelGenerator(item -> ((SampleItem) item).label());
+    }
+
+    private void setGridSampleData(Grid grid) {
+        grid.setItems(query -> samplePersonService.list(VaadinSpringDataHelpers.toSpringPageRequest(query)).stream());
+    }
+
+    @Autowired()
+    private SamplePersonService samplePersonService;
+}
